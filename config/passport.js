4445a5c18c5f04e8181d24f6ae4701c90fb2ed4b
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2013-07-29 22:30:34 -0700

    auth update.

--- config/passport.js
+++ config/passport.js
@@ -27,8 +27,8 @@ module.exports = function (passport, config) {
       usernameField: 'email',
       passwordField: 'password'
     },
-    function(email, password, done) {
-      User.findOne({ email: email }, function (err, user) {
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
         if (err) { return done(err) }
         if (!user) {
           return done(null, false, { message: 'Unknown user' })
